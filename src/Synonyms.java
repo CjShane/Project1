4fdc011af79525124df300c2c2b75cc5ea2b1e08
Cj Shane <cj34560@gmail.com>
2024-11-22 10:37:31 -0600

    Start

--- src/Synonyms.java
+++ src/Synonyms.java
@@ -1,15 +1,18 @@
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Scanner;
 public class Synonyms {
-    public Synonyms (URL[] corpus){
+    private HashMap<String, Integer> semantic;
+    public Synonyms (URL[] corpus, String consideredWord) {
+        semantic = new HashMap<>();
         parseCorpus(corpus);
     }
     public void parseCorpus(URL[] corpus){
         String delimtier = "[\\.\\?\\!]|\\Z";
         Scanner parser = new Scanner(System.in);
-        while(parser.hasNext()){
+        //while(parser.hasNext()){
             String line = parser.nextLine();
             String[] parts = line.split(delimtier);
-        }
+        //}
     }
 }
