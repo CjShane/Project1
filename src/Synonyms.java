69eeeafb7e94974421ea21cbe1356af2356c1dd2
Cj Shane <cj34560@gmail.com>
2024-12-03 21:02:46 -0600

    11/3

--- src/Synonyms.java
+++ src/Synonyms.java
@@ -1,17 +1,14 @@
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Synonyms {
-    private HashMap<Integer, String> semantic;
-    public Synonyms (URL[] corpus) throws IOException, MalformedURLException {
-        semantic = new HashMap<>();
+    private HashMap<String, HashMap<String, Integer>> descriptors;
+    public Synonyms (URL[] corpus, String base) throws IOException {
+        descriptors = new HashMap<>();
         parseCorpus(corpus);
     }
     public void parseCorpus(URL[] corpus) throws IOException {
@@ -28,19 +25,32 @@ public class Synonyms {
                     line = parser.next();
                     line = Arrays.toString(line.replaceAll("\\W+\\d+" , ""  ).toLowerCase().split("\\s+"));
                     line = line.replaceAll("[^a-zA-Z ]", "");
-                    semantic.put(count++, line);
+                    line = line.trim();
+                    for(int j = 0; j < line.length(); j++){
+                        String word = line.
+                        HashMap<String, Integer> map = new HashMap<>();
+                        if(!map.containsKey(word)){
+                            map.put(word, map.put(word, 1));
+                        }else{
+                            map.put(word, map.get(word) + 1);
+                        }
+
+                        descriptors.put(word, map);
+                    }
+                    //descriptors.put(count++, line );
                 }
             }catch(NoSuchElementException _) {}
         }
         while(count != 0) {
             //String words = semantic.get(count--);
             //words = words.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
-            System.out.println(semantic.get(count--));
+            System.out.println(descriptors.get(count--));
+            System.out.println();
         }
     }
-    public double calculateCosineSimilarity(String word1, String word2) {
-
-    }
+//    public double calculateCosineSimilarity(String word1, String word2) {
+//
+//    }
 }
 
 
